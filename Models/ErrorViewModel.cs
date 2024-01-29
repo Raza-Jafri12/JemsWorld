a6d53548377ad30031989fed52a284e7203c7196
Raza-Jafri12 <razahassan6275@gmail.com>
2024-01-29 14:04:46 +1000

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JemsWorld.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
